bba77d048256791448465ba5dc3877cc1d9d645b
Sanskar Lamsal <lamsal9677@yahoo.com>
2022-02-17 22:11:08 -0500

    PR Test

--- src/app.js
+++ src/app.js
@@ -3,11 +3,13 @@ import HeadShot from "./components/headshot";
 import MainBody from "./components/mainbody";
 import Footer from "./components/footer";
 
+//Making a simple change for the PR
 export default function App() {
   return (
     <div className="main">
       <HeadShot />
       <HeadShot />
+
       <MainBody />
       <Footer />
     </div>
